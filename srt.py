aea2d0afc90ed96b35087e9bdcba821afbd35107
Berlkot <91618103+Berlkot@users.noreply.github.com>
2022-05-17 14:12:46 +0300

    Update and rename aaaaaaaaaaaaa to srt.py

--- srt.py
+++ srt.py
@@ -2,20 +2,19 @@ def merge_sort(mas: list):
     sor = []
     for i in range(len(mas)):
         sor.append([mas[i]])
-    if len(sor)%2:
-        sor.append([])
     while len(sor) != 1:
         pod = []
         mas1, mas2 = sor.pop(0), sor.pop(0)
         for i in mas1:
             for j in mas2:
-                if i > j:
-                    pod.append(j)
-                elif mas2 != [] and mas1[-1] == i and i < j and j == mas2[-1]:
-                    pod.append(j)
+                s = j
+                if i > s:
+                    pod.append(s)
                 else:
                     break
             pod.append(i)
+            if mas1[-1] == i and i < s and s == mas2[-1]:
+                pod.append(j)
         sor.append(pod)
         print(sor)
     return(sor)
